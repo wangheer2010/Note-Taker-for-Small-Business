bdf7280fb16e30f7a54b8908ec94db136ccf1514
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-04-21 10:28:01 -0400

    Added functions

---
+++ server.js
@@ -0,0 +1,67 @@
+// load dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+// Reference the list of notes
+const allNotes = require('./db/db.json');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Join and save notes
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+// calls home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// calls notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Post new notes
+app.post('/api/notes', (req, res) => {
+    const allNotes =  JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNote = req.body;
+    newNote.id = uuid.v5();
+    allNotes.push(newNote);
+    fs.writeFileSync('./db/db.json',JSON.stringify(allNotes));
+    res.json(allNotes);
+});
+
+
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
+}
+
+// Delete notes
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+// Start Listing
+app.listen(PORT, () => {
+    console.log(`API listening on port: ${PORT}!`);
+});  
